bb9ee96dc44c76e885d649d9a92b85e08542827e
matt765 <mateusz.wyrebek@gmail.com>
2021-08-21 15:02:47 +0200

    api fix

--- js/converter.js
+++ js/converter.js
@@ -1,43 +1,28 @@
 const converterForm = document.getElementById('converterForm');
 
-converterForm.addEventListener('submit', function(e) {
+converterForm.addEventListener('submit', function (e) {
     e.preventDefault();
-
     let formValues = {};
-
     formValues.amount = document.getElementById('amount').value
-    formValues.firstCurrency =  document.getElementById('firstCurrency').value.toUpperCase(); 
-    formValues.secondCurrency = document.getElementById('secondCurrency').value.toUpperCase(); 
-   
-   convert(formValues);
-
-  
+    formValues.firstCurrency = document.getElementById('firstCurrency').value.toUpperCase();
+    formValues.secondCurrency = document.getElementById('secondCurrency').value.toUpperCase();
+    convert(formValues);
 }
 );
 
 
-
 function convert(formValues) {
-    
-    fetch('https://api.exchangeratesapi.io/latest?symbols=' + formValues.firstCurrency + '&base=' + formValues.secondCurrency)
-        .then(function (resp) { 
+    fetch(`https://free.currconv.com/api/v7/convert?q=${formValues.firstCurrency}_${formValues.secondCurrency}&compact=ultra&apiKey=b3fddab97074aef273d8`)
+        .then(function (resp) {
             return resp.json()
-         }) // Convert data to json
+        }) 
         .then(function (data) {
-            console.log(data);
-
-        
-  
-    let rate = "";
-    let x;
-    for (x in data.rates) {
-    rate = data.rates[x];
-  }
-  console.log(rate)
-           
-          
-           document.getElementById("conversionResult").innerHTML = (formValues.amount / rate).toFixed(2);
-          
+            let rate = "";
+            let x;
+            for (x in data) {
+                rate = data[x];
+            }
+            document.getElementById("conversionResult").innerHTML = (formValues.amount * rate).toFixed(2);
         })
         .catch(function () {
             // catch any errors
